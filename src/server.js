a5c5ed6554cdeb16867ba09831ebf6502fff2831
Rajeev <rajeev-choudhary@hotmail.com>
2019-04-28 22:38:25 +0530

    resolved deployment issues

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const http = require('http')
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/bank-search-app'));
+app.use(express.static(path.join(__dirname, 'dist/bank-search-app')));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/bank-search-app/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/bank-search-app/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
